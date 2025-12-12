c5ac3c8baf2eb2324b2ee1fb9a0c8f740567025b
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-12-12 03:50:11 +0300

    Update ProfileDetails.jsx

--- react-router-advanced/src/components/ProfileDetails.jsx
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -1,8 +1,29 @@
-export default function ProfileDetails() {
+
+import React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
+
+const Profile = () => {
   return (
     <div>
-      <h2>Profile Details</h2>
-      <p>Example information about the user.</p>
+      <h1>Profile Page</h1>
+
+      <nav>
+        <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
+      </nav>
+
+      {/* Nested routes rendered here */}
+      <Routes>
+        <Route index element={<ProfileDetails />} />
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+
+      {/* Optional Outlet if you want nested routing from App.jsx */}
+      <Outlet />
     </div>
   );
-}
+};
+
+export default Profile;
