55d6f5b6d9525a6789c0b5710730513ee7daa56a
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2025-12-12 03:37:29 +0300

    Update BlogPost.jsx

--- react-router-advanced/src/pages/BlogPost.jsx
+++ react-router-advanced/src/pages/BlogPost.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-export default function BlogPost() {
+const BlogPost = () => {
   const { postId } = useParams();
+  return <h2>Blog Post ID: {postId}</h2>;
+};
 
-  return (
-    <div>
-      <h1>Blog Post</h1>
-      <p>Showing post with id: <strong>{postId}</strong></p>
-      {/* In a real app, fetch post data using postId */}
-    </div>
-  );
-}
+export default BlogPost;
